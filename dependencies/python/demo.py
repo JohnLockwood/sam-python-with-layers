cc8e773f2eaab7c17781274dea518e09536bac62
John Lockwood <JohnLockwood2999@gmail.com>
2020-02-09 10:51:37 -0500

    Change target URL

--- dependencies/python/demo.py
+++ dependencies/python/demo.py
@@ -1,7 +1,8 @@
 import requests
 
 def readme():
-    resp = requests.get("https://raw.githubusercontent.com/JohnLockwood/getting-started-with-poetry/master/poetry-lambda-layer/README.rst")    
+    test_url = "https://raw.githubusercontent.com/JohnLockwood/sam-python-with-layers/master/tests/LayerSampleText.txt"
+    resp = requests.get(test_url)
     if resp.status_code == requests.status_codes.codes.OK :
         return resp.text
     else:
